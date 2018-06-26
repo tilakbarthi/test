87be60c34c2fa9d113fa0e34ec31ce88ace896ef
tilakbarthi <39404420+tilakbarthi@users.noreply.github.com>
2018-06-26 13:07:57 +0530

    Update hello.java

--- helo/src/com/jav/hello.java
+++ helo/src/com/jav/hello.java
@@ -6,5 +6,10 @@ public class hello {
 	System.out.println("my name is tisjhajshkjhdkjsahdkjashak");	System.out.println("my name is tisjhajshkjhdkjsahdkjashak");
 	
 	}
+	public static void main (String args[]){
+		System.out.println("my name is tilak");
+	System.out.println("my name is tisjhajshkjhdkjsahdkjashak");	
+	System.out.println("Done");	
+	}
 
 }
